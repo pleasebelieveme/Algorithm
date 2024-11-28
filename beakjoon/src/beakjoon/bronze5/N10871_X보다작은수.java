304ea6d0fff43f897a4b3bf0e1902e0c1b9c4592
pleasebelieveme <cain1227@naver.com>
2024-11-28 17:49:07 +0900

    20241128

--- beakjoon/src/beakjoon/bronze5/N10871_X보다작은수.java
+++ beakjoon/src/beakjoon/bronze5/N10871_X보다작은수.java
@@ -1,12 +1,16 @@
 package beakjoon.bronze5;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class N10871_X보다작은수 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        // 첫 번째 입력 받기
         Scanner sc = new Scanner(System.in);
 
-        // 첫 번째 입력 받기
         int N = sc.nextInt();  // 배열의 크기
         int X = sc.nextInt();  // 비교할 값
 
@@ -17,29 +21,59 @@ public class N10871_X보다작은수 {
             arr[i] = sc.nextInt();
         }
 
-        // arr을 사용하여 X보다 작은 값 출력
+        // Scanner를 사용하여 출력
+        printWithScanner(N, X, arr);
+
+        // StringBuilder를 사용하여 출력
+        sb(N, X, arr);
+
+        // BufferedReader와 StringTokenizer를 사용하여 출력
+        br(N, X, arr);
+
+        // Scanner 닫기
+        sc.close();
+    }
+
+    // Scanner를 사용하여 X보다 작은 값들만 출력하는 함수
+    public static void printWithScanner(int N, int X, int[] arr) {
         for (int i = 0; i < N; i++) {
             if (arr[i] < X) {
                 System.out.print(arr[i] + " ");
             }
         }
-
-        // 추가로 StringBuilder를 사용한 출력 예시
-        // sb(N, X, arr);
+        System.out.println();  // 줄바꿈
     }
 
     // StringBuilder를 사용하여 X보다 작은 값들만 출력하는 함수
     public static void sb(int N, int X, int[] arr) {
         StringBuilder sb = new StringBuilder();
-
-        // arr 배열에서 X보다 작은 값들 StringBuilder에 추가
         for (int i = 0; i < N; i++) {
             if (arr[i] < X) {
                 sb.append(arr[i]).append(" ");
             }
         }
+        System.out.println(sb.toString().trim());  // 마지막 공백 제거
+    }
 
-        // StringBuilder 결과 출력
+    // BufferedReader와 StringTokenizer를 사용하여 X보다 작은 값들만 출력하는 함수
+    public static void br(int N, int X, int[] arr) throws IOException {
+        // BufferedReader로 입력 받기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 첫 번째 줄은 이미 입력 받았기 때문에 두 번째 줄의 값들을 받아옵니다.
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        StringBuilder sb = new StringBuilder();
+
+        // X보다 작은 값들만 StringBuilder에 추가
+        for (int i = 0; i < N; i++) {
+            int value = Integer.parseInt(st.nextToken());
+            if (value < X) {
+                sb.append(value).append(' ');
+            }
+        }
+
+        // 결과 출력
         System.out.println(sb.toString().trim());  // 마지막 공백 제거
     }
 }
---
+++ beakjoon/src/beakjoon/bronze5/N10952.java
@@ -0,0 +1,16 @@
+package beakjoon.bronze5;
+
+import java.util.Scanner;
+
+public class N10952 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            if (A+B == 0) break;
+            System.out.println(A+B);
+        }
+    }
+}
