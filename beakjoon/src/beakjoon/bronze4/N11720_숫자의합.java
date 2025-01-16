b5c406bec9a7550f9490fe7d4e80d919872e4683
pleasebelieveme <cain1227@naver.com>
2025-01-16 22:30:50 +0900

    백준 브론즈5 10926 ??

--- beakjoon/src/beakjoon/bronze4/N11720_숫자의합.java
+++ beakjoon/src/beakjoon/bronze4/N11720_숫자의합.java
@@ -5,6 +5,7 @@ public class N11720_숫자의합 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        // 자리수가 너무 길면 int형으로 못 받을 수 있다.
         String sNum = sc.next();
         char[] cNum = sNum.toCharArray();
         int sum = 0;
@@ -12,5 +13,7 @@ public class N11720_숫자의합 {
             sum += cNum[i] - '0';
         }
         System.out.print(sum);
+        long l = Long.MAX_VALUE;
+        // System.out.println("l = " + l); // 9223372036854775807
     }
 }
---
+++ beakjoon/src/beakjoon/bronze5/N10926_아이디중복.java
@@ -0,0 +1,11 @@
+package beakjoon.bronze5;
+
+import java.util.Scanner;
+
+public class N10926_아이디중복 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		System.out.println(s + "??!");
+	}
+}
