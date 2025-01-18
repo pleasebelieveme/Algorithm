56aa1ab8dbd8db7e017b762287885491765e0365
pleasebelieveme <cain1227@naver.com>
2025-01-18 22:15:01 +0900

    얄코 알고리즘 11강 그래프

---
+++ problem/src/alco/L11_Graph.java
@@ -0,0 +1,11 @@
+package alco;
+
+/**
+ * 그래프는 vertex와 edge로 구성된 비선형 자료구조
+ * 활용 : 소셜네트워크, 지도, 컴퓨터네트워크, 웹페이지 링크구조
+ * vertex = node
+ * 노트를 이어주는게 edge
+ * 가중치 부여 가능
+ */
+public class L11_Graph {
+}
