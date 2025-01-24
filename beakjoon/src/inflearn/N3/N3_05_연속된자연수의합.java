66dcbe6e6dd8c0d3a6d0c635d66a4b09cda54f19
pleasebelieveme <cain1227@naver.com>
2025-01-24 22:03:22 +0900

    20250124 N3_06_연속된자연수의합

---
+++ beakjoon/src/inflearn/N3/N3_05_연속된자연수의합.java
@@ -0,0 +1,53 @@
+package inflearn.N3;
+
+import java.util.Scanner;
+
+public class N3_05_연속된자연수의합 {
+	private int solution(int n) {
+		int answer = 0;
+		int sum=0, lt=0;
+		int[] arr = new int[n];
+		for (int i = 1; i <= n; i++) {
+			arr[i-1] = i;
+		}
+		for (int rt = 0; rt < n; rt++) {
+			// 더하고
+			sum += arr[rt];
+			//확
+			if(sum==n) {
+				answer++;
+			}
+			while (sum >= n) {
+				sum -= arr[lt++];
+				if(sum==n) {
+					answer++;
+				}
+			}
+		}
+		return answer-1; // 마지막 숫자 빼기
+	}
+
+	// 강의풀이, n/2번만 하면돼!
+	public int solutionYou(int n) {
+		int answer = 0, sum = 0, lt = 0;
+		int m = n/2+1;
+		int[] arr = new int[m];
+		for (int i = 0; i < m; i++) arr[i] = i+1;
+		for (int rt = 0; rt < m; rt++) {
+			sum += arr[rt];
+			if(sum==n) answer++;
+			while (sum>=n) {
+				sum -= arr[lt++];
+				if(sum==n) answer++;
+			}
+		}
+
+		return answer;
+	}
+	public static void main(String[] args) {
+		N3_05_연속된자연수의합 T = new N3_05_연속된자연수의합();
+		Scanner kb = new Scanner(System.in);
+		int n = kb.nextInt();
+		System.out.println(T.solution(n));
+	}
+}
