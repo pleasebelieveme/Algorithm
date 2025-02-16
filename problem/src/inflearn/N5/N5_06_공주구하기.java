5ad66e8e67ab4b30e6d7a3088b00fe894d4010af
pleasebelieveme <cain1227@naver.com>
2025-02-16 09:13:15 +0900

    변경

---
+++ problem/src/inflearn/N5/N5_06_공주구하기.java
@@ -0,0 +1,18 @@
+package inflearn.N5;
+
+import java.util.Scanner;
+
+public class N5_06_공주구하기 {
+	public int solution(int n, int k) {
+		int answer = 0;
+		
+		return answer;
+	}
+	public static void main(String[] args) {
+		N5_06_공주구하기 T = new N5_06_공주구하기();
+		Scanner kb = new Scanner(System.in);
+		int n = kb.nextInt();
+		int k = kb.nextInt();
+		System.out.println(T.solution(n, k));
+	}
+}
