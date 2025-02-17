fd307ec0fe35657d2d0ecd969a1c7b7e328f5955
pleasebelieveme <cain1227@naver.com>
2025-02-17 22:15:01 +0900

    얄코 알고리즘 7강 이진트리

---
+++ problem/src/alco/L7_BinaryTree.java
@@ -0,0 +1,16 @@
+package alco;
+
+/**
+ * 이진트리의 종류
+ * 1. 이진 탐색 트리
+ * 2. AVL 트리
+ * 3. 레드-블랙 트리
+ *
+ * 순회방식
+ * preorder(1-2-3)
+ * inorder(2-1-3)
+ * postorder(2-3-1)
+ * levelorder - 위에서부터 아래 방향으로 순(큐사용)
+ */
+public class L7_BinaryTree {
+}
