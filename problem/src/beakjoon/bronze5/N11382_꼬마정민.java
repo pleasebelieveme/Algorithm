393e5a17e8a614abfcc81a3281f3854c293ff278
pleasebelieveme <cain1227@naver.com>
2025-02-21 19:52:15 +0900

    백준 N11382_꼬마정민 int안되고 long으로 타입지정

---
+++ problem/src/beakjoon/bronze5/N11382_꼬마정민.java
@@ -0,0 +1,13 @@
+package beakjoon.bronze5;
+
+import java.util.Scanner;
+
+public class N11382_꼬마정민 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long a = sc.nextLong();
+		long b = sc.nextLong();
+		long c = sc.nextLong();
+		System.out.println(a+b+c);
+	}
+}
