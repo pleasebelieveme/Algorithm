cc2563cb60d24840b61e11b5e47f7cf10b32a0da
believeme <cain1227@naver.com>
2025-06-19 22:29:06 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ problem/src/leetcode/0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -0,0 +1,9 @@
+SELECT 
+    ROUND(COUNT(DISTINCT a.player_id) * 1.0 / COUNT(DISTINCT b.player_id), 2) AS fraction
+FROM Activity a
+JOIN (
+    SELECT player_id, MIN(event_date) AS first_login
+    FROM Activity
+    GROUP BY player_id
+) b
+ON a.player_id = b.player_id AND a.event_date = DATE_ADD(b.first_login, INTERVAL 1 DAY);
