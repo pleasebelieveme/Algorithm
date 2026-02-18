9e628df8bac68ede35cdc33b3212b868462129a5
believeme <cain1227@naver.com>
2026-02-19 07:44:19 +0900

    N8_08_수열 변수명개선

--- problem/src/inflearn/N8/N8_08_수열추측하기.java
+++ problem/src/inflearn/N8/N8_08_수열추측하기.java
@@ -3,46 +3,51 @@ package inflearn.N8;
 import java.util.Scanner;
 
 public class N8_08_수열추측하기 {
-	static int[] b, permutation, check;
-	static int n, finalNumber;
-	boolean flag = false;
-	int[][] dy = new int[35][35];
-	public int combine(int n, int r) {
-		if(dy[n][r]>0) return dy[n][r];
-		if(n==r || r==0) return 1;
-		else return dy[n][r] = combine(n-1, r-1) + combine(n-1, r);
+	static int[] binomial, sequence, visited;
+	static int n, targetSum;
+	boolean found = false;
+	int[][] memo = new int[35][35];
+
+	public int combination(int n, int r) {
+		if(memo[n][r] > 0) return memo[n][r];
+		if(n == r || r == 0) return 1;
+		return memo[n][r] =
+			combination(n-1, r-1) + combination(n-1, r);
 	}
 
-	public void DFS(int level, int sum) {
-		if(flag) return;
-		if(level==n) {
-			if(sum==finalNumber) {
-				for(int x : permutation) System.out.print(x + " ");
-				flag = true;
+	public void DFS(int depth, int sum) {
+		if(found) return;
+
+		if(depth == n) {
+			if(sum == targetSum) {
+				for(int x : sequence)
+					System.out.print(x + " ");
+				found = true;
 			}
-		}
-		else {
-			for (int i = 1; i <=n ; i++) {
-				if(check[i]==0) {
-					check[i] = 1;
-					permutation[level] = i;
-					DFS(level+1, sum+(permutation[level]*b[level]));
-					check[i] = 0;
+		} else {
+			for(int i = 1; i <= n; i++) {
+				if(visited[i] == 0) {
+					visited[i] = 1;
+					sequence[depth] = i;
+					DFS(depth+1,
+						sum + sequence[depth] * binomial[depth]);
+					visited[i] = 0;
 				}
 			}
 		}
 	}
 
+
 	public static void main(String[] args) {
 		N8_08_수열추측하기 T = new N8_08_수열추측하기();
 		Scanner kb = new Scanner(System.in);
 		n = kb.nextInt();
-		finalNumber = kb.nextInt();
-		b = new int[n];
-		permutation = new int[n];
-		check = new int[n+1];
+		targetSum = kb.nextInt();
+		binomial = new int[n];
+		sequence = new int[n];
+		visited = new int[n+1];
 		for (int i = 0; i < n; i++) {
-			b[i] = T.combine(n-1, i);
+			binomial[i] = T.combination(n-1, i);
 		}
 		T.DFS(0, 0);
 	}
