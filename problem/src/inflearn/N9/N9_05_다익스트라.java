3d884a76cbe9afb0822d2a6c0ba8d6aea7b1212b
believeme <cain1227@naver.com>
2026-03-08 22:08:23 +0900

    20260308 N9_05_다익스트라

---
+++ problem/src/inflearn/N9/N9_05_다익스트라.java
@@ -0,0 +1,65 @@
+package inflearn.N9;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class N9_05_다익스트라 {
+	static class Edge implements Comparable<Edge> {
+		public int vex; // 정점
+		public int cost; // 가중치 값
+		Edge(int vex, int cost) {
+			this.vex = vex;
+			this.cost = cost;
+		}
+		@Override
+		public int compareTo(Edge ob) {
+			return this.cost - ob.cost;
+		}
+	}
+
+	static int n, m;
+	static ArrayList<ArrayList<Edge>> graph;
+	static int[] distance;
+	public void solution(int v) {
+		PriorityQueue<Edge> pQ = new PriorityQueue<>();
+		pQ.offer(new Edge(v, 0));
+		distance[v] = 0;
+		while (!pQ.isEmpty()) {
+			Edge tmp = pQ.poll();
+			int now = tmp.vex;
+			int nowCost = tmp.cost;
+			for(Edge ob : graph.get(now)) {
+				if(distance[ob.vex] > nowCost+ob.cost) {
+					distance[ob.vex] = nowCost+ob.cost;
+					pQ.offer(new Edge(ob.vex, nowCost+ob.cost));
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		N9_05_다익스트라 T = new N9_05_다익스트라();
+		Scanner kb = new Scanner(System.in);
+		n = kb.nextInt(); // 정점의 갯수
+		m = kb.nextInt(); // 간선의 갯수
+		graph = new ArrayList<ArrayList<Edge>>();
+		for(int i=0; i<=n; i++) {
+			graph.add(new ArrayList<Edge>());
+		}
+		distance = new int[n+1];
+		Arrays.fill(distance, Integer.MAX_VALUE);
+		for(int i=0; i<m; i++) {
+			int a = kb.nextInt();
+			int b = kb.nextInt();
+			int c = kb.nextInt();
+			graph.get(a).add(new Edge(b, c));
+		}
+		T.solution(1);
+		for(int i=2; i<=n; i++) {
+			if(distance[i] != Integer.MAX_VALUE) System.out.println(i + " : " + distance[i]);
+			else System.out.println(i + " : impossible");
+		}
+	}
+}
