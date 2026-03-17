a0d1bee51f0acd980ceb8d244ef20d2d497c951a
believeme <cain1227@naver.com>
2026-03-17 15:47:30 +0900

    N10_03_최대부분증가수열

---
+++ problem/src/inflearn/N10/N10_03_최대부분증가수열.java
@@ -0,0 +1,30 @@
+package inflearn.N10;
+
+import java.util.Scanner;
+
+public class N10_03_최대부분증가수열 {
+	public int solution(int[] arr) {
+		int[] dy = new int[arr.length];
+		int answer = 0;
+		dy[0] = 1;
+		for(int i=1; i<arr.length; i++) {
+			int max = 0;
+			for(int j=i-1; j>=0; j--) {
+				if(arr[j]<arr[i] && dy[j]>max) max = dy[j];
+			}
+			dy[i] = max+1;
+			answer = Math.max(answer, dy[i]);
+		}
+		return answer;
+	}
+	public static void main(String[] args) {
+		N10_03_최대부분증가수열 T = new N10_03_최대부분증가수열();
+		Scanner kb = new Scanner(System.in);
+		int n = kb.nextInt();
+		int arr[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = kb.nextInt();
+		}
+		System.out.println(T.solution(arr));
+	}
+}
