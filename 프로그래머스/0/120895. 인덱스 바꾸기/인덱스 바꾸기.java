fb952feb05d508c782b5b7eb0c74e845c8697d3f
pleasebelieveme <cain1227@naver.com>
2026-05-09 21:58:41 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.02 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        char[] ch = my_string.toCharArray();
+        char temp = ch[num1];
+        ch[num1] = ch[num2];
+        ch[num2] = temp;
+        return String.valueOf(ch);
+    }
+}
