90cf30f1d4fedad03178b66d506bde6730ec72ce
pleasebelieveme <cain1227@naver.com>
2026-05-11 23:30:54 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 5.24 ms, Memory: 59.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String my_string) {
+        return my_string.toLowerCase().chars().sorted()
+               .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+               .toString();
+    }
+}
