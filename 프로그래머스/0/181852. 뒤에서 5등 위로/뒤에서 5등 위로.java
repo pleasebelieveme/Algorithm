2726b32645b5eae1bf837d9bb25fdf7256abfbd9
pleasebelieveme <cain1227@naver.com>
2026-05-23 18:44:47 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.29 ms, Memory: 64.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length-5];
+        int idx = 0;
+        Arrays.sort(num_list);
+        for(int i=5; i<num_list.length; i++) {
+            answer[idx] = num_list[i];
+            idx++;
+        }
+        return answer;
+    }
+}
