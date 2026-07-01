d3682d647b7552bc4e46a47afb717c5077fdadc7
pleasebelieveme <cain1227@naver.com>
2026-07-02 08:47:34 +0900

    [level 0] Title: 할 일 목록, Time: 0.03 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+class Solution {
+    public ArrayList<String> solution(String[] todo_list, boolean[] finished) {
+        ArrayList<String> answer = new ArrayList<>();
+        for(int i=0; i<todo_list.length; i++) {
+            if(finished[i]==false) {
+                answer.add(todo_list[i]);
+            }
+        }
+        return answer;
+    }
+}
