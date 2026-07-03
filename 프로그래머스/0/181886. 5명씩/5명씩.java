afe5744cb44a2633521ab68ccdc203cb0c79d6fb
pleasebelieveme <cain1227@naver.com>
2026-07-03 20:41:26 +0900

    [level 0] Title: 5명씩, Time: 0.01 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+class Solution {
+    public String[] solution(String[] names) {
+        String[] answer = new String[(names.length + 4)/5];      
+        int idx = 0;
+        for (int i = 0; i < names.length; i+=5) {
+            answer[idx] = names[i];
+            idx++;
+        }
+        
+        return answer;
+    }
+}
