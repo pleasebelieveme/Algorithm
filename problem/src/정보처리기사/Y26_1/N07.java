65c3cfd7ba4891ff82f43b96340ec968a7f14077
believeme <cain1227@naver.com>
2026-07-09 19:15:22 +0900

    정보처리기사 26년 1회 7번문제

--- problem/src/정보처리기사/Y26_1/N07.java
+++ problem/src/정보처리기사/Y26_1/N07.java
@@ -2,7 +2,7 @@ package 정보처리기사.Y26_1;
 
 public class N07 {
 
-	// static을 붙여 독립적인 구조로 만들어 줍니다.
+	// static을 붙여 독립적인 구조로 생성
 	static class A {
 		String f(Object x) {
 			return "1";
@@ -16,14 +16,13 @@ public class N07 {
 		String f(Object x) {
 			return "2";
 		}
-		String f(String x) {
+		String f(String x) { // 부모타입이면 자식에서만 정의한 메서드는 사용 불가
 			return "3";
 		}
 	}
 
-	// 파일명을 따라 N07 클래스 내부에 main 메서드를 위치시킵니다.
 	public static void main(String[] args) {
 		A a = new B();
-		System.out.println(a.g()); // 실행 시 정확하게 2가 출력됩니다.
+		System.out.println(a.g()); // 2
 	}
 }
