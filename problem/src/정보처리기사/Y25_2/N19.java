48e667fbb9635323f0cc0d393b140d1964e7c0a1
believeme <cain1227@naver.com>
2026-07-13 12:13:41 +0900

    정보처리기사 25년 2회 19번 문제

---
+++ problem/src/정보처리기사/Y25_2/N19.java
@@ -0,0 +1,25 @@
+package 정보처리기사.Y25_2;
+
+public class N19 {
+	public static class BO {
+		public int v;
+		public BO(int v) {
+			this.v = v;
+		}
+	}
+
+	public static void main(String[] args) {
+		BO a = new BO(1);
+		BO b = new BO(2);
+		BO c = new BO(3);
+		BO[] arr = {a, b, c};
+
+		BO t = arr[0];
+		arr[0] = arr[2];
+		arr[2] = t;
+
+		arr[1].v = arr[0].v;
+
+		System.out.println(a.v + "a" + b.v + "b" + c.v);
+	}
+}
