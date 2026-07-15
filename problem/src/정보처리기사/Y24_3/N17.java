35928870cbf4e09f840f48e599c3c9d0d9c2fee4
believeme <cain1227@naver.com>
2026-07-16 08:14:37 +0900

    정보처리기사 24년 3회 17번 문제

---
+++ problem/src/정보처리기사/Y24_3/N17.java
@@ -0,0 +1,24 @@
+package 정보처리기사.Y24_3;
+
+public class N17 {
+	public static void main(String[] args) {
+		int sum = 0;
+		try {
+			func();
+		}
+		catch (NullPointerException e) {
+			sum = sum + 1;
+		}
+		catch (Exception e) {
+			sum = sum + 10;
+		}
+		finally {
+			sum = sum + 100;
+		}
+		System.out.print(sum);
+	}
+
+	static void func() throws Exception {
+		throw new NullPointerException();
+	}
+}
