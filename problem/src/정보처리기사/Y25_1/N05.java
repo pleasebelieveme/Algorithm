9f88829187d20e090cec980199a6aed5c214fec8
believeme <cain1227@naver.com>
2026-07-15 17:32:40 +0900

    정보처리기사 25년 1회 5번 문제

---
+++ problem/src/정보처리기사/Y25_1/N05.java
@@ -0,0 +1,25 @@
+package 정보처리기사.Y25_1;
+
+public class N05 {
+	public static void main(String[] args) {
+		int a = 5, b = 0;
+		try {
+			System.out.print(a/b);
+		}
+		catch(ArithmeticException e){
+			System.out.print("출력1");
+		}
+		catch(ArrayIndexOutOfBoundsException e) {
+			System.out.print("출력2");
+		}
+		catch(NumberFormatException e) {
+			System.out.print("출력3");
+		}
+		catch(Exception e) {
+			System.out.print("출력4");
+		}
+		finally {
+			System.out.print("출력5");
+		}
+	}
+}
