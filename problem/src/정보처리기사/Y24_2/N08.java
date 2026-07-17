00e79c11d41e6ba9a3731a926e62f866d96b9417
believeme <cain1227@naver.com>
2026-07-17 23:27:16 +0900

    정보처리기사 24년 2회 8번 문제

---
+++ problem/src/정보처리기사/Y24_2/N08.java
@@ -0,0 +1,21 @@
+package 정보처리기사.Y24_2;
+
+public class N08 {
+	public static String rf(String str, int index, boolean[] seen) {
+		if (index < 0) return "";
+		char c = str.charAt(index);
+		String result = rf(str, index - 1, seen);
+		if (!seen[c]) {
+			seen[c] = true;
+			return c + result;
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		String str = "abacabcd";
+		int len = str.length();
+		boolean[] seen = new boolean[256];
+		System.out.print(rf(str, len - 1, seen));
+	}
+}
