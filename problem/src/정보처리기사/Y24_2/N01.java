2c5e64f344fd1589d59819dda4a53dcd6b8438e4
believeme <cain1227@naver.com>
2026-07-17 22:20:36 +0900

    정보처리기사 24년 2회 1번 문제

---
+++ problem/src/정보처리기사/Y24_2/N01.java
@@ -0,0 +1,9 @@
+package 정보처리기사.Y24_2;
+
+public class N01 {
+	public static void main(String[] args) {
+		String str = "ITISTESTSTRING";
+		String[] result = str.split("T");
+		System.out.print(result[3]);
+	}
+}
