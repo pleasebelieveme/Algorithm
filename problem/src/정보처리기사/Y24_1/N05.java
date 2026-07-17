625f2376edfac74b86fca9d4f63520ce4c47f5b7
believeme <cain1227@naver.com>
2026-07-18 00:03:46 +0900

    정보처리기사 24년 1회 5번 문제

---
+++ problem/src/정보처리기사/Y24_1/N05.java
@@ -0,0 +1,30 @@
+package 정보처리기사.Y24_1;
+
+class Connection {
+	private static Connection _inst = null;
+	private int count = 0;
+
+	public static Connection get() {
+		if (_inst == null) {
+			_inst = new Connection();
+			return _inst;
+		}
+		return _inst;
+	}
+
+	public void count() { count++; }
+	public int getCount() { return count; }
+}
+
+public class N05 {
+	public static void main(String[] args) {
+		Connection conn1 = Connection.get();
+		conn1.count();
+		Connection conn2 = Connection.get();
+		conn2.count();
+		Connection conn3 = Connection.get();
+		conn3.count();
+		conn1.count();
+		System.out.print(conn1.getCount());
+	}
+}
