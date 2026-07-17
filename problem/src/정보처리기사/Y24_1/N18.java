4cdb0e06cbb2264ca108d44965383081e60d1522
believeme <cain1227@naver.com>
2026-07-18 00:05:55 +0900

    정보처리기사 24년 1회 18번 문제

---
+++ problem/src/정보처리기사/Y24_1/N18.java
@@ -0,0 +1,29 @@
+package 정보처리기사.Y24_1;
+
+class firstArea {
+	int x, y;
+	public firstArea(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	public void print() {
+		System.out.println(x + y);
+	}
+}
+
+class secondArea extends firstArea {
+	int bb = 3;
+	public secondArea(int i) {
+		super(i, i + 1);
+	}
+	public void print() {
+		System.out.println(bb * bb);
+	}
+}
+
+public class N18 {
+	public static void main(String[] args) {
+		firstArea st = new secondArea(10);
+		st.print();
+	}
+}
